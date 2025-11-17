77f40d43f289d0f8acfe110ebd54abfb4dc32d04
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 <ezequiel@stamm.bio>
2025-11-16 23:25:20 -0300

    removing unnecesary files

--- work/24/73e42696c38c92e6dea10d21a25b59/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-gatk HaplotypeCaller             -R ref.fasta             -I reads_mother.bam             -O reads_mother.bam.vcf             -L intervals.bed
--- work/ce/a66237a7d2e7fb1d3fcff126e2bce9/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-gatk HaplotypeCaller             -R ref.fasta             -I reads_son.bam             -O reads_son.bam.vcf             -L intervals.bed
